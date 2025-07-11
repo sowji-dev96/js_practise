6f0dd6060bdaa2edadc518e34caf9eb6b1da7894
sowji-dev96 <gsowjanyalatha@gmail.com>
2025-07-12 00:35:30 +0530

    new

--- js_practise1.js
+++ js_practise1.js
@@ -1,16 +1,35 @@
-const reverStr = (str) => {
-   return str.split("").reverse().join("")
-}
+// const reverStr = (str) => {
+//    return str.split("").reverse().join("")
+// }
 
-console.log(reverStr("BenAndSowji"));
-
-
-const isPalindrome  = (str)=> {
-    return str.split("").reverse().join("") === str
-}
+// console.log(reverStr("BenAndSowji"));
 
 
-console.log(isPalindrome("wow"))
+// const isPalindrome  = (str)=> {
+//     return str.split("").reverse().join("") === str
+// }
 
-console.log(isPalindrome("sowji"))
 
+// console.log(isPalindrome("wow"))
+
+// console.log(isPalindrome("sowji"))
+
+// var a=20;
+// var b=28;
+// document.write ("result:", a + b)
+
+const arr = ["a","b","c"]
+
+
+
+
+
+const obj = {a:"1",b:"2", c:"3"}
+
+// obj[a] or obj.a
+
+//obj[b] or obj.b
+
+
+let subjects=["eng","mat",28,{name :"sowji"}]
+console.log(subjects)
