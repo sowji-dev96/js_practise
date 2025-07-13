f4d69e18358be9ea71040d0df1cfa55509d962d1
sowji-dev96 <gsowjanyalatha@gmail.com>
2025-07-13 10:15:44 +0530

    all  sample programs

--- js_practise1.js
+++ js_practise1.js
@@ -31,5 +31,141 @@ const obj = {a:"1",b:"2", c:"3"}
 //obj[b] or obj.b
  
 
-let subjects=["eng","mat",28,{name :"sowji"}]
-console.log(subjects)
+// let subjects=["eng","mat",28,{name :"sowji"}]
+ //console.log(subjects)
+
+ //var  a = 60;
+ //a++
+  
+ //var  b = 16;
+ //b++
+
+ //document.write("result:",a)
+
+ //var  a = 60;
+ //a --
+  
+ //var  b = 16;
+ //b--
+
+ //document.write("result:", b) 
+
+ //var game = "cricket";
+
+ //let  captain = "virat"
+
+ //const  tennis = "sania"
+
+ //if (g ame == "something") {
+   // document.write("match captain is", captain)
+ //} else if(tinnis == "sania") {
+   // document.write("correct")
+ //} else {
+  //document.write("nothing is matching")
+//}     
+
+var x = 100;
+var y = 50;
+var z = 200;
+ 
+//if (x == y) {
+  //  document.write("x is equal to y")
+//} else if (y > z) {
+  //  document.write("blue colour")
+//} else if (z < x) {
+  //  document.write("yellow colour")
+//} else {
+  //  document.write("all wrong statements")
+//}
+ 
+//var x = 100;
+ //var y =  50;
+ //var z =  200;
+
+ //if (x == y && y < z )  {
+   // document.write("yes  correct answer");
+ //} else if ((x + y) < z ) {
+   //  document.write(" yellow color is printed")
+ //}
+
+ //var game = "cricket";
+  
+ //let caption = "dhoni";
+   
+ //const tennis = "sania"
+
+ //if (game == "something") {
+   // document.write("match captain is ",captain)
+
+ //} else if (tennis == "sania") {
+   // document.write("correct")
+ //} else {
+   // document.write("nothing  is matching")
+ //}
+ //document.write(
+  //  2 + 3 == 5 ? "yes  correct answer ": " no wrong answer" 
+
+ //)
+ //var x = 100;
+ //var y = 50;
+ //x < y ?  document.write("color is green") : document.write("red color")
+
+ //alert("this is an alert")
+//prompt("please  enter your  name")
+
+//if  (10 == 20) {
+  //  alert("10 is less  then 20")   
+//}
+// var women = confirm("are you a women")
+  //if(women){
+    //let name = prompt("please enter")
+    //document.write("hello madam."+ name)
+  //} else{
+//let name = prompt("please enter your name")
+  //}
+
+  //var  a = "55";
+  //var  b = "44";
+  //var  z = parseInt(a)
+  //var  x = string(b)
+
+//document.write(typeof x +"<br>")
+//document.write(typeof z)
+
+let  nname = prompt("please enter your name");
+let  marks = prompt("enter your marks");
+ 
+ switch (true) 
+ {
+    case  marks> 90 && marks <=100:
+     result = "great you are the topper"    
+    break;
+    case marks>65  &&  marks  <= 90:
+      result = "congratulations you got first class"
+    break;
+    case marks >40 &&  marks <65:
+       result = "you got second class" 
+    break;
+    case  marks >=  30 && marks <= 40:
+       result = "you got third class" 
+    break;
+    case  marks < 30 :
+        result =" you are faild ,better luck next time"
+    break;
+     default:
+     result = "please enter your marks"
+     break    
+ }
+ document.write(result)
+  
+
+
+
+
+
+
+
+
+
+
+
